2541942bdc175db50d47fa84ea4c6c1e01034ab7
Etienne Mustow <etienne.o.mustow@gmail.com>
2020-03-23 14:14:26 +0000

    headlines can be found by searchTerm

--- app.js
+++ app.js
@@ -1,50 +1,113 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-const request = require("./request");
 var router = express.Router();
 require("dotenv").config();
 var bodyParser = require("body-parser");
-var url = "http://api.ft.com/content/search/v1?" + 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
+
   res.render("index", { title: "Home" });
 });
 
 
-app.get('/search', function(req, res, next) {
+app.get('/search', function (req, res, next) {
   res.render("searches", { title: "Searches" });
   var queryString;
   queryString = req.body.queryString;
   console.log(queryString);
- });
+  var articles = makeArticleRequestForQuery(queryString);
+  console.log(articles);
+  module.exports = articles
+});
 
-app.post('/search/', function(req, res, next) {
+app.post('/search/', function (req, res, next) {
   var queryString;
   queryString = req.body.queryString;
   res.send(queryString);
   console.log("STRING" + queryString);
-  console.log(req.body);
+  // console.log(req.body);
+  var articles = makeArticleRequestForQuery(queryString);
+})
 
-});
-
-
-app.get("/", function(req, res) {
-  console.log("you've arrived");
-});
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-request.sendRequestFor("Brexit");
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
 });
 
 module.exports = router;
+
+
+// var request = require('request');
+
+// //set url
+// var url = "http://api.ft.com/content/search/v1?";
+
+// //set header
+// var headers = {
+//   'Content-Type': 'application/javascript',
+//   'x-api-key': process.env.FT_API_KEY,
+// };
+
+// //set form data
+// var form = {queryString: queryString};
+
+// //set request parameter
+// request.post({headers: headers, url: url, form: form, method: 'POST'}, function (e, r, body) {
+
+//     var bodyValues = JSON.parse(body);
+//     res.send(bodyValues);
+// });
+
+function makeArticleRequestForQuery(queryString) {
+  var request = require('request');
+  request({
+    headers: {
+      'x-api-key': "59cbaf20e3e06d3565778e7b39ea4ee200cd4785bdcf1c69154f84d7",
+    },
+    url: "http://api.ft.com/content/search/v1?",
+    json: {
+      "queryString": 'title:\"' + queryString + '\"', 
+      "resultContext": {
+        "aspects": ["title", "lifecycle", "location", "summary", "editorial"],
+        "sortOrder": "DESC",
+        "sortField": "initialPublishDateTime"
+      }
+    },
+    method: 'POST'
+  }, function (err, res, body) {
+    articles = body["results"]
+    indexCount = articles[0]["indexCount"]
+    articlesArray = articles[0]["results"]
+
+    // for (let index = 0; index < indexCount.length; index++) {
+    //   var element = articlesArray[index];
+    //   var MongoClient = require('mongodb').MongoClient;
+    //   var url = "mongodb://localhost:27017/";
+    //   MongoClient.connect(url, function (err, db) {
+    //     if (err) throw err;
+    //     var dbo = db.db("mydb");
+    //     var myobj = element;
+    //     dbo.collection("articles").insertOne(myobj, function (err, res) {
+    //       if (err) throw err;
+    //       console.log("1 document inserted");
+    //       db.close();
+    //     });
+    //   });
+    // }
+
+    console.log(articles);
+    console.log(articles[0]["indexCount"])
+    console.log(articles[0]["results"][0])
+    console.log(articles[0]["results"][1])
+  });
+}
