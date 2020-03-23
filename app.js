f1909d500f24b28daf23c335132710ca8dec1b96
Etienne Mustow <etienne.o.mustow@gmail.com>
2020-03-23 10:25:48 +0000

    post action somewhat working

--- app.js
+++ app.js
@@ -2,22 +2,36 @@ var express = require("express");
 var app = express();
 var path = require("path");
 const request = require("./request");
+var router = express.Router();
 require("dotenv").config();
 var bodyParser = require("body-parser");
+var url = "http://api.ft.com/content/search/v1?" + 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.urlencoded());
+
+
 app.get("/", function(req, res) {
   res.render("index", { title: "Home" });
 });
 
-app.post("/search", (req, res) => {
-  const searchTerm = req.body.queryString;
-  //...
-  res.end();
-  console.log(searchTerm);
+
+app.get('/search', function(req, res, next) {
+  res.render("searches", { title: "Searches" });
+  var queryString;
+  queryString = req.body.queryString;
+  console.log(queryString);
+ });
+
+app.post('/search/', function(req, res, next) {
+  var queryString;
+  queryString = req.body.queryString;
+  res.send(queryString);
+  console.log("STRING" + queryString);
+  console.log(req.body);
+
 });
+
 
 app.get("/", function(req, res) {
   console.log("you've arrived");
@@ -32,3 +46,5 @@ request.sendRequestFor("Brexit");
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
 });
+
+module.exports = router;
