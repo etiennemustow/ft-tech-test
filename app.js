b672c0440e8595a8517d2303b43096f2720e14fb
Etienne Mustow <etienne.o.mustow@gmail.com>
2020-03-24 11:37:37 +0000

    pushing db setup to heroku

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
+require("dotenv").config();
 var express = require("express");
 var app = express();
 var path = require("path");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-
-require("dotenv").config();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/intense-shore-82818');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.listen(process.env.PORT || 5000, function () {
+  if(process.env.PORT){
+    console.log("Example app listening on port" + process.env.PORT + "!");
+  } 
+  else {
+  console.log("Example app listening on port 5000!");
+  }
+});
 
 
 app.get("/", function (req, res) {
 
   res.render("index", { title: "Home" });
 });
-
 
 app.get('/search', function (req, res, next) {
   var resultsArray = []
@@ -46,19 +59,6 @@ app.post('/search/', function (req, res, next) {
   makeArticleRequestForQuery(queryString);
   res.redirect('/search');
 })
-
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-app.listen(process.env.PORT || 5000, function () {
-  console.log("Example app listening on port 3000!");
-});
-
-
-
 
 function makeArticleRequestForQuery(queryString) {
   var request = require('request');
